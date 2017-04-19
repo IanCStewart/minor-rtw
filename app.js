51332069d8bec937cd480ed28e0fb3e7cb5632e3
iancstewart <ian.c.stewart@icloud.com>
2017-04-19 14:21:04 +0200

    :sparkles: Added idex routing.

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ app.use(express.static('public', { maxAge: '31d' }))
   .set('view engine', 'jsx')
   .engine('jsx', require('express-react-views').createEngine());
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => {
+  if (req.app.accesToken) {
+    // User has auth redirect to main screen
+    res.redirect('/player');
+  } else {
+    // No auth yet render index with sing in button
+    res.render('pages/index');
+  }
+});
+
+app.get('/player', (req, res) => res.send('Welcome to the player'));
 
 app.listen(port, host, () => {
   console.log(`Server running ${host}:${port}`); // eslint-disable-line no-console
